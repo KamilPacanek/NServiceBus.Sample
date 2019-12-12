fcc16822c998b3d069ac7793a60f2e44826708bf
Kamil Pacanek <kamil.pacanek@gmail.com>
2019-12-12 08:31:58 +0000

    Add test LearningTransport transport

--- NServiceBus.Sample/ClientUI/Program.cs
+++ NServiceBus.Sample/ClientUI/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NServiceBus;
+using System;
 using System.Threading.Tasks;
 
 namespace ClientUI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "ClientUI";
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
         }
     }
 }
